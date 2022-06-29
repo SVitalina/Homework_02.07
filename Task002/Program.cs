787a15a20f59ff701c1863fe932a57dcf6256ac4
Oleksii <zankexd@icloud.com>
2022-06-29 19:11:27 +0200

    Task 2

---
+++ Task002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 2 числа : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if ( a < b)
+{
+    Console.WriteLine("min : " + a + " max : " + b );
+}
+if (a > b)
+{
+    Console.WriteLine("min : " + b + " max : " + a );
+}
