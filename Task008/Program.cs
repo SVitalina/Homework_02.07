f8fe87309d132bcd252aa9d7722739979cf54999
Oleksii <zankexd@icloud.com>
2022-06-29 19:05:18 +0200

    Task 8

---
+++ Task008/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите числo : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int index = 0; 
+
+while ( index < a-1 ) 
+{
+    index +=2 ;
+    Console.WriteLine( index );
+}
