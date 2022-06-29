2b8a4de424033b40b330a4cbb2a7459a27c8fbbd
Oleksii <zankexd@icloud.com>
2022-06-29 19:06:25 +0200

    Task 4

---
+++ Task004/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите 3 числа : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b )
+{ 
+    if (a > c)
+    {
+        Console.WriteLine("max: " + a);
+    }
+}
+if (a < b )
+{ 
+    if (b > c)
+    {
+        Console.WriteLine("max: " + b);
+    }
+}
+if (c > a )
+{ 
+    if (c > b)
+    {
+        Console.WriteLine("max: " + c);
+    }
+}
